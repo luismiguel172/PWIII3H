f9cbe25a60482c3b6eae667a0cd0b8dd134bca7f
luismiguel172 <luis.goncalves68@etec.sp.gov.br>
2023-09-04 21:23:00 -0300

    att objetos

--- Objetos3.php
+++ Objetos3.php
@@ -2,4 +2,19 @@
 
     $conta_carlos = new Conta;
     $conta_carlos->Agencia = 6677;
-    $conta_carlos->
+    $conta_carlos->Codigo = "CC.1234.56";
+    $conta_carlos->DataDeCriacao = "10/07/02";
+    $conta_carlos->Titular = $carlos;
+    $conta_carlos->Senha = 9876;
+    $conta_carlos->Saldo = 567.89;
+    $conta_carlos->Cancelada = false;
+
+    echo "\n";
+    echo "Manipulando a conta de: {$conta_carlos->Titular->Nome} \n";
+    echo "O saldo atual é R\$ {$conta_carlos->ObterSaldo()} \n";
+
+    $conta_carlos->Depositar(20);
+    echo "O saldo atual é R\$ {$conta_carlos->ObterSaldo()} \n";
+
+    $conta_carlos->Retirar(10);
+    echo "O saldo atual é R\$ {$conta_carlos->ObterSaldo()} \n";
